0cbb2333cc3bee55d194e7634b5e2454cf3c49c6
Akinnurun Oluwafemi <akinnurun.samuel@gmail.com>
2020-06-19 14:45:57 +0100

    added assets and Makefile

--- main.go
+++ main.go
@@ -2,24 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
 
-// The new router function creates the router and
-// returns it to us. We can now use this function
-// to instantiate and test the router outside of the main function
 func newRouter() *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/hello", handler).Methods("GET")
+
+	// Declare the static file directory and point it to the
+	// directory we just made
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./assets/")))
+
+
 	return r
 }
 
 func main() {
+	// Initialize Logger
+	logger := log.New(os.Stdout, "web-app ", log.LstdFlags)
+	logger.Println("Starting the application...")
+
 	// The router is now formed by calling the `newRouter` constructor function
 	// that we defined above. The rest of the code stays the same
 	r := newRouter()
+
+	// listens on the TCP network address addr
 	http.ListenAndServe(":8080", r)
 }
 
